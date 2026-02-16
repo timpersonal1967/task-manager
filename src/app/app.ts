0f14e16e5a53e75aeec10891b246b9493022985e
tim.daniels.dev <tim.daniels.dev@outlookc.om>
2026-02-16 15:44:36 +0000

    Initial commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';   
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HttpClientModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
