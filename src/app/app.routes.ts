1c8e4d45c95c95a08b234c9116a01b53cdf7b367
tim.daniels.dev <tim.daniels.dev@outlookc.om>
2026-02-19 12:42:32 +0000

    styling improvement from vibe coding with my copilot

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { TaskListComponent } from './tasks/task-list/task-list';
 import { TaskFormComponent } from './tasks/task-form/task-form';
+import { About } from './about/about';
 
 export const routes: Routes = [
   { path: '', component: TaskListComponent },
   { path: 'add-task', component: TaskFormComponent },
+  { path: 'about', component: About },
   { path: 'edit-task/:id', component: TaskFormComponent }
 ];
 
